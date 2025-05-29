9f1e1be677eec35f06d51bfce91ca06dd3d3b693
micabod <167199490+micabod@users.noreply.github.com>
2025-05-29 11:56:18 -0300

    nh

---
+++ Assets/deteccioncolciones.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class deteccioncolciones : MonoBehaviour
+{
+   void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Contacto");
+        Destroy(collision.gameObject);
+    }
+}
+
